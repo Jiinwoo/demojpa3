0b166fe8c6d556068520a5752950964a3e0fdadb
jinwoojung <jwjung5038@gmail.com>
2020-03-20 00:08:51 +0900

    queryDSL 추가

--- src/main/java/me/jung/demojpa3/Post/PostRepository.java
+++ src/main/java/me/jung/demojpa3/Post/PostRepository.java
@@ -1,7 +1,8 @@
 package me.jung.demojpa3.Post;
 
 import me.jung.demojpa3.MyRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface PostRepository extends MyRepository<Post, Long>, PostCustomRepository {
+public interface PostRepository extends MyRepository<Post, Long>, PostCustomRepository, QuerydslPredicateExecutor<Post> {
 
 }
--- src/test/java/me/jung/demojpa3/Post/PostRepositoryTest.java
+++ src/test/java/me/jung/demojpa3/Post/PostRepositoryTest.java
@@ -1,6 +1,7 @@
 package me.jung.demojpa3.Post;
 
 
+import com.querydsl.core.types.Predicate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,6 +25,17 @@ public class PostRepositoryTest {
     @Autowired
     ApplicationContext applicationContext;
 
+    @Test
+    public void crud() {
+        Post post = new Post();
+        post.setContent("hi jinwoo");
+        postRepository.save(post.publish());
+
+        Predicate predicate = QPost.post.content.containsIgnoreCase("Hi");
+        Optional<Post> one = postRepository.findOne(predicate);
+        assertThat(one).isNotEmpty();
+    }
+
 //    @Test
 //    public void event(){
 //        Post post = new Post();
@@ -31,20 +45,20 @@ public class PostRepositoryTest {
 //        applicationContext.publishEvent(event);
 //    }
 
-    @Test
-    public void crud() {
-        Post post = new Post();
-        post.setContent("hibernate");
-
-        assertThat(postRepository.contains(post)).isFalse();
-
-        postRepository.save(post.publish());
-        assertThat(postRepository.contains(post)).isTrue();
-
-
-//        postRepository.findMyPost();
-        postRepository.delete(post);
-        postRepository.flush();
-    }
+//    @Test
+//    public void crud() {
+//        Post post = new Post();
+//        post.setContent("hibernate");
+//
+//        assertThat(postRepository.contains(post)).isFalse();
+//
+//        postRepository.save(post.publish());
+//        assertThat(postRepository.contains(post)).isTrue();
+//
+//
+////        postRepository.findMyPost();
+//        postRepository.delete(post);
+//        postRepository.flush();
+//    }
 
 }
